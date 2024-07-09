cc257838aeffc39d51002ee74154f92bcd3f074a
Yoel Castillo <yscc0002@ce.pucmm.edu.do>
2024-07-09 15:41:41 -0400

    Another one

--- ProyectoFinal/src/logico/MotherBoard.java
+++ ProyectoFinal/src/logico/MotherBoard.java
@@ -1,6 +1,9 @@
 package logico;
 
+import java.util.ArrayList;
+
 public class MotherBoard {
+	
 	private String numeroDeSerie;
 	private float precio;
 	private int cantDisponible;
@@ -9,6 +12,67 @@ public class MotherBoard {
 	private String tipoDeConector;
 	private String tipoDeMemoriaRam;
 	private ArrayList<DiscoDuro> discoDurosAceptados;
-	// v3
+	
+	public MotherBoard(String numeroDeSerie, float precio, int cantDisponible, String marca, String modelo,
+			String tipoDeConector, String tipoDeMemoriaRam, ArrayList<DiscoDuro> discoDurosAceptados) {
+		super();
+		this.numeroDeSerie = numeroDeSerie;
+		this.precio = precio;
+		this.cantDisponible = cantDisponible;
+		this.marca = marca;
+		this.modelo = modelo;
+		this.tipoDeConector = tipoDeConector;
+		this.tipoDeMemoriaRam = tipoDeMemoriaRam;
+		this.discoDurosAceptados = discoDurosAceptados;
+	}
+	
+	public float getPrecio() {
+		return precio;
+	}
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+	public int getCantDisponible() {
+		return cantDisponible;
+	}
+	public void setCantDisponible(int cantDisponible) {
+		this.cantDisponible = cantDisponible;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public String getTipoDeConector() {
+		return tipoDeConector;
+	}
+	public void setTipoDeConector(String tipoDeConector) {
+		this.tipoDeConector = tipoDeConector;
+	}
+	public String getTipoDeMemoriaRam() {
+		return tipoDeMemoriaRam;
+	}
+	public void setTipoDeMemoriaRam(String tipoDeMemoriaRam) {
+		this.tipoDeMemoriaRam = tipoDeMemoriaRam;
+	}
+	public ArrayList<DiscoDuro> getDiscoDurosAceptados() {
+		return discoDurosAceptados;
+	}
+	public void setDiscoDurosAceptados(ArrayList<DiscoDuro> discoDurosAceptados) {
+		this.discoDurosAceptados = discoDurosAceptados;
+	}
+	public String getNumeroDeSerie() {
+		return numeroDeSerie;
+	}
+	
+	
+
 
 }
